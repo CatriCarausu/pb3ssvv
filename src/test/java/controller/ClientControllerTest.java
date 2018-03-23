f0ad135f2cf73f5a6e306f381aea0fc1e61e991a
ASUSUser <carausu.catrinel@gmail.com>
2018-03-23 19:06:21 +0200

    Initial commit

---
+++ src/test/java/controller/ClientControllerTest.java
@@ -0,0 +1,31 @@
+package controller;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Catri on 23-Mar-18.
+ */
+public class ClientControllerTest {
+    ClientController controller;
+
+    @Before
+    public void setUp() throws Exception {
+        controller = new ClientController();
+    }
+
+    @Test
+    public void addClient() throws Exception {
+    }
+
+    @Test
+    public void addClientIndex() throws Exception {
+    }
+
+    @Test
+    public void listIssue() throws Exception {
+    }
+
+}
